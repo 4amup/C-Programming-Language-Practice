abe01842e05165f5b56517034fd726fc6f39815a
lixiaochun <490398311@qq.com>
2019-12-03 21:19:48 +0800

    update week08 01

--- week08/01/main.c
+++ week08/01/main.c
@@ -5,22 +5,31 @@ int GetApple(int a[], int height, int n);
 
 int main(int argc, char const *argv[])
 {
-    int a[N],
-        height,
-        n;
+    int a[N];
+    int height = 0, n = 0;
 
-    for (int i = 0; i <N; i++)
+    for (int i = 0; i < N; i++)
     {
         scanf("%d", &a[i]);
     }
-    
-    height = scanf("%d", &height);
 
-    n = GetApple(a, height, n);
+    scanf("%d", &height);
+
+    n = GetApple(a, height, N);
+    printf("%d", n);
     return 0;
 }
 
 int GetApple(int a[], int height, int n)
 {
-    return 10;
+    int count = 0;
+    height += 30;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] <= height)
+        {
+            count++;
+        }
+    }
+    return count;
 }
